26c9b985df7dedaebf7e03f5773d151f588a04e6
yeonvv <yeonvv8@gmail.com>
2022-07-16 20:03:22 +0900

    use-inheritance-object

---
+++ use-inheritance-object.js
@@ -0,0 +1,32 @@
+const kim = {
+  name: "kim",
+  first: 10,
+  second: 20,
+  sum: function () {
+    return this.first + this.second;
+  },
+};
+
+const lee = Object.create(kim);
+lee.name = "lee";
+lee.first = 10;
+lee.second = 10;
+lee.avg = function () {
+  return (this.first + this.second) / 2;
+};
+// const lee = {
+//   name: "lee",
+//   first: 10,
+//   second: 10,
+//   avg: function () {
+//     return (this.first + this.second) / 2;
+//   },
+// };
+// lee.__proto__ = kim;
+
+console.group("object useInheritance");
+
+console.log("kim.sum() :", kim.sum());
+console.log("lee.sum() :", lee.avg());
+
+console.groupEnd("object useInheritance");
